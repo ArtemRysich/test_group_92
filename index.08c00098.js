24e2e4ac8a8dec52a94f2db494543aae9d0b5d45
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2023-10-03 18:11:59 +0000

    Deploying to gh-pages from @ ArtemRysich/test_group_92@ca64f2b301d4f7c6e4c7282f10ff495bf8644962 🚀

---
+++ index.08c00098.js
@@ -0,0 +1,2 @@
+!function(){value=10,console.log(value);var l=value+10;console.log(l)}();
+//# sourceMappingURL=index.08c00098.js.map
---
+++ index.5a93c304.js
@@ -0,0 +1,2 @@
+value=10,console.log(10);console.log(20);
+//# sourceMappingURL=index.5a93c304.js.map
